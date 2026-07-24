b74d35c0fc26efb19402e6c4b5e239524be13b63
zhaowcheng <zhaowcheng@163.com>
2026-07-25 00:40:29 +0800

    release v0.1.1

--- setup.py
+++ setup.py
@@ -1,67 +1,32 @@
-# Copyright (c) 2025-2026, zhaowcheng <zhaowcheng@163.com>
+# Build hook: read version from xflow/framework/version.py,
+# generate long_description from README files.
+# All other metadata lives in pyproject.toml.
+import re
 
-import os
-
-from setuptools import setup, find_namespace_packages
-
-
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+from setuptools import setup
 
 
-def find_version():
-    verfile = os.path.join(BASEDIR, 'xflow', 'framework', 'version.py')
-    with open(verfile, 'r', encoding='utf8') as f:
-        for line in f.readlines():
-            if line.startswith('__version__'):
-                return line.split('=')[1].strip().strip("'").strip('"')
+_VERSION_FILE = "xflow/framework/version.py"
 
 
-def find_requires():
-    reqfile = os.path.join(BASEDIR, 'requirements.txt')
-    with open(reqfile, 'r', encoding='utf8') as f:
-        return [r.strip() for r in f.readlines()]
-    
+def _find_version() -> str:
+    with open(_VERSION_FILE, encoding="utf8") as f:
+        m = re.search(r'''__version__\s*=\s*['"]([^'"]+)''', f.read())
+    return m.group(1) if m else "0.0.0"
 
-def find_long_description():
-    with open('README.md', encoding='utf8') as f:
+
+def _build_long_description(version: str) -> str:
+    with open("README.md", encoding="utf8") as f:
         desc = f.read()
-    desc = desc.replace('/blob/master/',
-                        f'/blob/v{find_version()}/')
+    desc = desc.replace("/blob/master/", f"/blob/v{version}/")
+    desc = desc.replace("/tree/master/", f"/tree/v{version}/")
     return desc
 
 
+_VERSION = _find_version()
+
 setup(
-    name='xflow.framework',
-    version=find_version(),
-    description='可以用 Python 写 Pipeline 的 CICD 框架。',
-    long_description=find_long_description(),
-    long_description_content_type='text/markdown',
-    author='zhaowcheng',
-    author_email='zhaowcheng@163.com',
-    install_requires=find_requires(),
-    packages=find_namespace_packages(exclude=['tests', 'tests.*']),
-    entry_points={
-        'console_scripts': [
-            'xflow = xflow.framework.main:main'
-        ]
-    },
-    include_package_data=True,
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12"
-    ],
-    url='https://github.com/zhaowcheng/xflow.framework',
-    python_requires='>=3.6',
-    project_urls={
-        'Homepage': 'https://github.com/zhaowcheng/xflow.framework',
-        'Issues': 'https://github.com/zhaowcheng/xflow.framework/issues'
-    }
+    version=_VERSION,
+    long_description=_build_long_description(_VERSION),
+    long_description_content_type="text/markdown",
 )
--- xflow/framework/version.py
+++ xflow/framework/version.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2025-2026, zhaowcheng <zhaowcheng@163.com>
+# Single source of truth for version.
+# Both pyproject.toml (via setup.py) and runtime CLI read from here.
 
-
-__version__ = '0.1.0'
+__version__ = '0.1.1'
